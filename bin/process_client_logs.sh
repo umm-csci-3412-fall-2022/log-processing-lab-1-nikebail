f58fd47a29eacb5b59f4cdc3f9532841f4ffa2bb
Mason Eischens <eisch115@sputnik.morris.umn.edu>
2022-09-13 17:15:24 -0500

    working on frist task for lab

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,1 +1,10 @@
-#! usr/bin/bash
+#!/bin/bash
+
+cd $1
+here=$(pwd)
+
+for file in $here/var/log/*
+do
+echo "$file"
+	awk 'match($0, /(\w{3}) (\S+) (\w+):\S+ (\w+?) .+? Failed password [^0-9]+([0-9.]+/, failedLoginData) {print failedLoginData[1] " " failedLoginData[2] " " failedLoginData[3] " " failedLoginData[4] " " failedLoginData[5] "\n" }' < "$file" >> failed_login_data.txt
+done
