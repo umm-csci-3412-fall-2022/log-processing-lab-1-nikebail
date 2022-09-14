9ac17a0b9bea227682dee15325baab9d1668f39a
Nik Bailey <bail0301@morris.umn.edu>
2022-09-14 09:21:52 -0500

    code seems to work in the command line but not when we run the actual tests, might as for Peter's help again

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -6,5 +6,5 @@ here=$(pwd)
 for file in $here/var/log/*
 do
 echo "$file"
-	awk 'match($0, /(\w{3}) (\S+) (\w+):\S+ (\w+?) .+? Failed password [^0-9]+([0-9.]+/, failedLoginData) {print failedLoginData[1] " " failedLoginData[2] " " failedLoginData[3] " " failedLoginData[4] " " failedLoginData[5] "\n" }' < "$file" >> failed_login_data.txt
+	awk 'match($0, /(\w{3}) (\S+) (\w+):\S+ (\w+?) .+? Failed password .+ from ([0-9.]+)/, failedLoginData) {print failedLoginData[1] " " failedLoginData[2] " " failedLoginData[3] " " failedLoginData[4] " " failedLoginData[5] "\n" }' < "$file" >> failed_login_data.txt
 done
