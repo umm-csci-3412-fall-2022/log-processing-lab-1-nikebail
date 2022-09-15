f0b166d4b1cfd46ec68886045c945d0e6164c63b
Mason Eischens <eisch115@sputnik.morris.umn.edu>
2022-09-15 18:45:52 -0500

    fixed issue with second regex expression failing to match certain numerical names, added lines to remove temp files/move username_dist.html into the correct directory

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -6,10 +6,16 @@ echo "$file"
 done
 sort temp.txt > sorted_temp.txt
 uniq -c sorted_temp.txt >> freq_name.txt
-rm temp.txt
 while read line
 do
 echo "$line" > super_temp_file.txt
-	awk 'match($0, /(\d+) (\S+)/, name_freq) {print "data.addRow([" name_freq[2] ", " name_freq[1] "]);"}' < super_temp_file.txt >> content.html
+	awk 'match($0, /(\S+) (\S+)/, name_freq) {print "data.addRow([" name_freq[2] ", " name_freq[1] "]);"}' < super_temp_file.txt >> content.txt
 done < freq_name.txt
-./bin/wrap_contents.sh content.html html_components/username_dist username_dist.html
+
+./bin/wrap_contents.sh content.txt html_components/username_dist username_dist.html
+mv username_dist.html data
+# Clean up!
+rm temp.txt
+rm sorted_temp.txt
+rm freq_name.txt
+rm super_temp_file.txt
