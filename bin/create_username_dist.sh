f3ed5dddccbdd4845c9e285ef647e9c13e99b739
Mason Eischens <eisch115@sputnik.morris.umn.edu>
2022-09-15 16:25:20 -0500

    bugfixing script

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -2,13 +2,14 @@
 for file in "$1"/*/failed_login_data.txt
 do
 echo "$file"
-	awk 'match($0, /\w{3} {1,2}\S+ \w+ (\w+)/, names) {print "\047" names[1] "\047"}' < "$file" >> "$1"/temp.txt
+	awk 'match($0, /\w{3} {1,2}\S+ \w+ (\w+)/, names) {print "\047" names[1] "\047"}' < "$file" >> temp.txt
 done
 sort temp.txt > sorted_temp.txt
-uniq -c sorted_temp.txt
+uniq -c sorted_temp.txt >> freq_name.txt
 rm temp.txt
 while read line
 do
-	awk 'match($0, /(\d+) (\w+)/, name_freq) {print "data.addRow([" name_freq[2] ", " name_freq[1] "]);"}' < "$line" >> content.html
-done < sorted_temp.txt
+echo "$line" > super_temp_file.txt
+	awk 'match($0, /(\d+) (\S+)/, name_freq) {print "data.addRow([" name_freq[2] ", " name_freq[1] "]);"}' < super_temp_file.txt >> content.html
+done < freq_name.txt
 ./bin/wrap_contents.sh content.html html_components/username_dist username_dist.html
