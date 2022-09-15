fe8984f1f82de3987f88a53acdf7e698f06fab92
Mason Eischens <eisch115@sputnik.morris.umn.edu>
2022-09-15 16:02:24 -0500

    editted create_username_dist, added regex match expressions to extract nessecary data

--- bin/create_username_dist.sh
+++ bin/create_username_dist.sh
@@ -1,1 +1,14 @@
-#! usr/bin/bash
+#!/bin/bash
+for file in "$1"/*/failed_login_data.txt
+do
+echo "$file"
+	awk 'match($0, /\w{3} {1,2}\S+ \w+ (\w+)/, names) {print "\047" names[1] "\047"}' < "$file" >> "$1"/temp.txt
+done
+sort temp.txt > sorted_temp.txt
+uniq -c sorted_temp.txt
+rm temp.txt
+while read line
+do
+	awk 'match($0, /(\d+) (\w+)/, name_freq) {print "data.addRow([" name_freq[2] ", " name_freq[1] "]);"}' < "$line" >> content.html
+done < sorted_temp.txt
+./bin/wrap_contents.sh content.html html_components/username_dist username_dist.html
