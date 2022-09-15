37dc6c2ac37346c3438e647b73b730ab2ff3c103
mason-eisch <mason@LAPTOP-I0DHCDMQ.localdomain>
2022-09-15 00:29:11 -0500

    altered regex expression to grab username instead of computer name

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -6,5 +6,5 @@ here=$(pwd)
 for file in $here/var/log/*
 do
 echo "$file"
-	awk 'match($0, /(\w{3}) (\S+) (\w+):\S+ (\w+?) .+? Failed password .+ from (\S.+) port/, failedLoginData) {print failedLoginData[1] " " failedLoginData[2] " " failedLoginData[3] " " failedLoginData[4] " " failedLoginData[5] "\n" }' < "$file" >> failed_login_data.txt
+	awk 'match($0, /(\w{3}) (\S+) (\w+):.+? Failed password for.+? (\S+) from (\S+)/, failedLoginData) {print failedLoginData[1] " " failedLoginData[2] " " failedLoginData[3] " " failedLoginData[4] " " failedLoginData[5]}' < "$file" >> failed_login_data.txt
 done
