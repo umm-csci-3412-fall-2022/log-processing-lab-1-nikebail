3d16a55d02942541960a36997eec98a9bcab5bd2
Mason Eischens <eisch115@sputnik.morris.umn.edu>
2022-09-15 15:09:51 -0500

    fixed formatting to comply with shellcheck

--- bin/process_client_logs.sh
+++ bin/process_client_logs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cd $1
+cd "$1" || exit
 here=$(pwd)
 
-for file in $here/var/log/*
+for file in "$here"/var/log/*
 do
 echo "$file"
 	awk 'match($0, /(\w{3}) {1,2}(\S+) (\w+):.+? Failed password .{1,16} (\S+) from (\S+)/, failedLoginData) {print failedLoginData[1] " " failedLoginData[2] " " failedLoginData[3] " " failedLoginData[4] " " failedLoginData[5]}' < "$file" >> failed_login_data.txt
