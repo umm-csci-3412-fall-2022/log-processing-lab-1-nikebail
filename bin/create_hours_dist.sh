6486540628873a350c9d1950c900f6813fb0750c
Nik Bailey <bail0301@morris.umn.edu>
2022-09-20 11:30:16 -0500

    Copy-pasted code from create_username_dist.sh into create_hours_dist.sh, changed some of the code to be for create_hours_dist.sh, Peter I know that copy-paste is the death of understanding but rememeber I'm lazy

--- bin/create_hours_dist.sh
+++ bin/create_hours_dist.sh
@@ -1,2 +1,32 @@
 #!/bin/bash
 
+#iterate through all versions of failed_login_data in the subdirectories of the main directory (represented by $1)
+for file in "$1"/*/failed_login_data.txt
+do
+echo "$file"
+	awk 'match($0, /\w{3} {1,2}\S+ \w+ (\w+)/, names) {print "\047" names[1] "\047"}' < "$file" >> temp.txt
+done
+
+# Sorts names for uniq
+sort temp.txt > sorted_temp.txt
+
+# Writes usernames/number of failed login attempts to a temp file
+uniq -c sorted_temp.txt >> freq_name.txt
+
+# Extract names/number of failed login attempts temp file, formats the data as a javaScript method call, writes output to another temp file
+while read -r line
+do
+echo "$line" > super_temp_file.txt
+	awk 'match($0, /(\S+) (\S+)/, name_freq) {print "data.addRow([" name_freq[2] ", " name_freq[1] "]);"}' < super_temp_file.txt >> content.txt
+done < freq_name.txt
+
+# Wrap the data in the HTML header/footers 
+./bin/wrap_contents.sh content.txt html_components/hours_dist hours_dist.html
+mv hours_dist.html data
+
+# Clean up temp files!
+rm content.txt
+rm temp.txt
+rm sorted_temp.txt
+rm freq_name.txt
+rm super_temp_file.txt
