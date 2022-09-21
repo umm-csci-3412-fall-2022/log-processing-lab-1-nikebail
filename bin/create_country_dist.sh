7d8beac102afe0e91c72a7416a64c83c6ac54350
Nik Bailey <bail0301@morris.umn.edu>
2022-09-21 08:36:04 -0500

    Added code to the create_country to count the occurences of each IP address

--- bin/create_country_dist.sh
+++ bin/create_country_dist.sh
@@ -10,8 +10,13 @@ done
 # Sort the IP addresses numerically 0-9
 sort temp.txt > sorted_temp.txt
 
-# Get the correct country codes for each P address
-$join temp.txt etc/country_IP_map.txt > country_IP.txt
+# Counts the occurence of each IP address
+uniq -c sorted_temp.txt >> freq_countries.txt
+
+# Get the correct country codes for each IP address
+# Should create a file where each line is number of occurences of
+# that an IP adress, followed by the IP address and then the country code
+$join freq_countries.txt etc/country_IP_map.txt > country_IP.txt
 
 # Wrap the data in the HTML header/footers
 ./bin/wrap_contents.sh content.txt html_components/username_dist username_dist.html
