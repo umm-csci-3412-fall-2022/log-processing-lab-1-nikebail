0d7bbfd0efecf3c22256bba5ed60b99438d9a8df
Nik Bailey <bail0301@morris.umn.edu>
2022-09-20 21:45:41 -0500

    Made join in create_country_dist, may or may not work

--- bin/create_country_dist.sh
+++ bin/create_country_dist.sh
@@ -7,6 +7,12 @@ echo "$file"
 	awk 'match($0, /([0-9]+\..+)/, names) {print "\047" names[1] "\047"}' < "$file" >> temp.txt
 done
 
+# Sort the IP addresses numerically 0-9
+sort temp.txt > sorted_temp.txt
+
+# Get the correct country codes for each P address
+$join temp.txt etc/country_IP_map.txt > country_IP.txt
+
 # Wrap the data in the HTML header/footers
 ./bin/wrap_contents.sh content.txt html_components/username_dist username_dist.html
 mv username_dist.html data
