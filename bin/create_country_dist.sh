b31659802d490f034807afd572e6107df2fc1fbf
Nik Bailey <bail0301@morris.umn.edu>
2022-09-20 21:24:20 -0500

    Power broke
    Power went out but our changes we saved!!

--- bin/create_country_dist.sh
+++ bin/create_country_dist.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 
+#iterate through all versions of failed_login_data in the subdirectories of the main directory (represented by $1)
+for file in "$1"/*/failed_login_data.txt
+do
+echo "$file"
+	awk 'match($0, /([0-9]+\..+)/, names) {print "\047" names[1] "\047"}' < "$file" >> temp.txt
+done
+
+# Wrap the data in the HTML header/footers
+./bin/wrap_contents.sh content.txt html_components/username_dist username_dist.html
+mv username_dist.html data
