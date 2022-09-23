48870acaddfd886f51169cfc9fcf9283b0cba946
Mason Eischens <eisch115@sputnik.morris.umn.edu>
2022-09-22 19:06:30 -0500

    restructuring process_logs.sh

--- bin/process_logs.sh
+++ bin/process_logs.sh
@@ -1,9 +1,18 @@
-#! usr/bin/bash
-mkdir tempdir
+#!/bin/bash
+currentdir=$(pwd)
+tempdir="/tmp/logs_processing_temp"
+mkdir -p -v "$tempdir"
 for var in "$@"
-do 
-	tar -xf "$var" -C /tempdir
-	./bin/process_client_logs.sh ./tempdir/
+do  
+	workingdir="$tempdir"/"${var%.*}"
+	mkdir -p -v "$workingdir"
+	tar -xf "$var" --directory "$workingdir"
+	./bin/process_client_logs.sh "$workingdir"
 done
+./bin/create_username_dist.sh "$tempdir"
+./bin/create_hours_dist.sh "$tempdir"
+./bin/create_country_dist.sh "$tempdir"
 
- 
+./bin/assemble_report.sh "$tempdir"
+
+mv failed_login_summary.html "$currentdir" 
