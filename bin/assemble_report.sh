087bafa06f108f35468b58c8766f71298f1aa640
Mason Eischens <eisch115@sputnik.morris.umn.edu>
2022-09-22 21:29:25 -0500

    commented code/cleaned up formatting for shellcheck

--- bin/assemble_report.sh
+++ bin/assemble_report.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+# concatenate the contents of the three distributions
 cat "$1"/country_dist.html "$1"/hours_dist.html "$1"/username_dist.html > temp.txt
+
+#wraps distributions in the overall header/footer for the final webpage
 ./bin/wrap_contents.sh temp.txt html_components/summary_plots failed_login_summary.html
+
+#moves to data :D 
 mv failed_login_summary.html data 
--- bin/process_logs.sh
+++ bin/process_logs.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+
+# saves current directory in variable 
 currentdir=$(pwd)
+
+# creates temp directory for processing 
 tempdir="/tmp/logs_processing_temp"
 mkdir data
 mkdir -p "$tempdir"
+
+# iterates through all arguments, unzips them into their own directory within the tempdirectory, generates failed_login_data using process_client_logs.sh
 for var in "$@"
 do  
 	workingdir="$tempdir"/"${var%.*}"
@@ -10,14 +16,19 @@ do
 	tar -xf "$var" --directory "$workingdir"
 	./bin/process_client_logs.sh "$workingdir"
 done
-ls "$tempdir"/log_files/zeus_secure
+
 processed_logs="$tempdir"/log_files
+
+# runs our three processing scripts on the failed login data
 ./bin/create_username_dist.sh "$processed_logs"
 ./bin/create_hours_dist.sh "$processed_logs"
 ./bin/create_country_dist.sh "$processed_logs"
 
+# assembles all the html files into a final webpage
 ./bin/assemble_report.sh "$currentdir"/data
 
+# moves back into the directory in which the script was called
 mv data/failed_login_summary.html "$currentdir"
 
+# Clean up: removes temp directory
 rm -rf "$tempdir"
